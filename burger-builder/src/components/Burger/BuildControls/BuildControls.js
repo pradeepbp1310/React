75582fd117954200da6230632525a49401fafbe0
Pradeep B P <pradeepbp1310@gmail.com>
2019-06-27 15:30:33 +0530

    Added Build controls for burger

---
+++ burger-builder/src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './BuildControls.css'
+
+const buildControls = () => {
+    return (
+        <div className={styles.BuildControls}></div>
+    )
+}
+
+export default buildControls;
