9340c69eda534754c6f31b29c55485971f068767
Pradeep B P <pradeepbp1310@gmail.com>
2019-07-01 11:59:29 +0530

    Improving performance by implementing shouldComponentUpdate lifecycle

--- burger-builder/src/components/Burger/OrderSummary/OrderSummary.js
+++ burger-builder/src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,23 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Aux from '../../../hoc/Auxiliary';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = (props) => {
-    const ingredientList = Object.keys(props.ingredient).map(igKey => {
-        return <li key={igKey}><span style={{ textTransform: 'capitalize' }}>{igKey}</span>: {props.ingredient[igKey]}</li>
-    })
-    return (
-        <Aux>
-            <h1>Your Order Summary</h1>
-            <p>Your Burger contains following ingredients!</p>
-            <ul>
-                {ingredientList}
-            </ul>
-            <p><strong>Total price: {props.totalprice}</strong></p>
-            <Button btnType={'Danger'} clicked={props.closemodal}>Cancel</Button>
-            <Button btnType={'Success'} clicked={props.closemodal}>Continue</Button>
-        </Aux>
-    )
+class OrderSummary extends Component {
+    // Order summary page renders each time when any of the parent page renders
+    // to avoid this implement componentshouldupdate lifecycle in parent wrapped class
+    componentWillUpdate() {
+        console.log('OrderSummary component');
+    }
+
+    render() {
+        const ingredientList = Object.keys(this.props.ingredient).map(igKey => {
+            return <li key={igKey}><span style={{ textTransform: 'capitalize' }}>{igKey}</span>: {this.props.ingredient[igKey]}</li>
+        })
+        return (
+            <Aux>
+                <h1>Your Order Summary</h1>
+                <p>Your Burger contains following ingredients!</p>
+                <ul>
+                    {ingredientList}
+                </ul>
+                <p><strong>Total price: {this.props.totalprice}</strong></p>
+                <Button btnType={'Danger'} clicked={this.props.closemodal}>Cancel</Button>
+                <Button btnType={'Success'} clicked={this.props.closemodal}>Continue</Button>
+            </Aux>
+        )
+    }
 }
 
-export default orderSummary;
+export default OrderSummary;
--- burger-builder/src/components/UI/Modal/Modal.js
+++ burger-builder/src/components/UI/Modal/Modal.js
@@ -1,24 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classes from './Modal.module.css';
 import Aux from '../../../hoc/Auxiliary';
 import Backdrop from '../Backdrop/Backdrop';
 
-const modal = (props) =>
-    (
-        <Aux>
-            <Backdrop show={props.show} clicked={props.closemodal} />
-            <div className={classes.Modal}
-                style={
-                    {
-                        transform: props.show ? 'TranslateY(0)' : 'TranslateY(-100vh)',
-                        opacity: props.show ? '1' : '0'
+class Modal extends Component {
+// Only update when there is a change in the props
+    shouldComponentUpdate(nextProps){
+        return nextProps.show !== this.props.show;
+    }
+
+    render() {
+        return (
+            <Aux>
+                <Backdrop show={this.props.show} clicked={this.props.closemodal} />
+                <div className={classes.Modal}
+                    style={
+                        {
+                            transform: this.props.show ? 'TranslateY(0)' : 'TranslateY(-100vh)',
+                            opacity: this.props.show ? '1' : '0'
+                        }
                     }
-                }
-            >
-                {props.children}
-            </div>
-        </Aux>
+                >
+                    {this.props.children}
+                </div>
+            </Aux>
 
-    );
+        );
+    }
+}
 
-export default modal;
+export default Modal;
